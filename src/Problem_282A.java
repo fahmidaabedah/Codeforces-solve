6db8f3ace6a21600a318b79e678a6be8805451e6
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-05-19 13:22:12 +0600

    solved problem

---
+++ src/Problem_282A.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem_282A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int testcases=sc.nextInt();
+        sc.nextLine();
+        int x=0;
+        while(testcases-->0){
+            String Input=sc.nextLine();
+            if(Input.equals("X++")){
+                x++;
+            }
+            if(Input.equals("X--")){
+                x--;
+            }
+            if(Input.equals("++X")){
+                ++x;
+            }
+            if(Input.equals("--X")){
+                --x;
+            }
+        }
+        System.out.println(x);
+    }
+}
