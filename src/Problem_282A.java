9e710a1ecd3cfec178a27ed2f46af6fc7d1c1cfe
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-05-19 13:24:12 +0600

    solved problem

--- src/Problem_282A.java
+++ src/Problem_282A.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Problem_282A {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int testcases=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
         sc.nextLine();
-        int x=0;
-        while(testcases-->0){
-            String Input=sc.nextLine();
-            if(Input.equals("X++")){
+        int x = 0;
+        while (testcases-- > 0) {
+            String Input = sc.nextLine();
+            if (Input.equals("X++")) {
                 x++;
             }
-            if(Input.equals("X--")){
+            if (Input.equals("X--")) {
                 x--;
             }
-            if(Input.equals("++X")){
+            if (Input.equals("++X")) {
                 ++x;
             }
-            if(Input.equals("--X")){
+            if (Input.equals("--X")) {
                 --x;
             }
         }
