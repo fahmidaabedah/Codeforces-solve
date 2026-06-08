20082fa22c327e696558ed3b52966960300eb027
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-06-08 23:14:36 +0600

    solved problem

---
+++ src/Problem_734A.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem_734A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String gameWin = sc.next();
+        int anton = 0;
+        int danik = 0;
+        for (int i = 0; i < gameWin.length(); i++) {
+            if (gameWin.charAt(i) == 'A') {
+                anton++;
+            } else if (gameWin.charAt(i) == 'D') {
+                danik++;
+            }
+        }
+//        System.out.println(anton);
+//        System.out.println(danik);
+        if (anton > danik) {
+            System.out.println("Anton");
+        } else if (anton < danik) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+    }
+}
