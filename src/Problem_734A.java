8faa9b26c42074f005d862858865a17c83e9ff84
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-06-09 10:49:32 +0600

    solved problem

--- src/Problem_734A.java
+++ src/Problem_734A.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Problem_734A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
         String gameWin = sc.next();
         int anton = 0;
         int danik = 0;
-        for (int i = 0; i < gameWin.length(); i++) {
+        for (int i = 0; i < size; i++) {
             if (gameWin.charAt(i) == 'A') {
                 anton++;
             } else if (gameWin.charAt(i) == 'D') {
