76d0cc4160dfd5b8606018783b73b8da6fa0b4b8
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-07-15 22:22:20 +0600

    solved problem

---
+++ src/Problem_2246A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem_2246A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i += 2) {
+                arr[i] = i + 2;
+                arr[i + 1] = i + 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
